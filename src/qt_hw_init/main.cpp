417869ebae545c707b4e8bc2b0bc503760009df4
Rainer Keller <rainer.keller@digia.com>
2013-10-17 16:02:27 +0200

    Show audio level before and after change
    Change-Id: I0a63b2abec22308fb522e2c1c8347e4cc45ad424
    Reviewed-by: Yoann Lopes <yoann.lopes@digia.com>

--- src/qt_hw_init/main.cpp
+++ src/qt_hw_init/main.cpp
@@ -1,4 +1,5 @@
 #include "../utils/qdroidutils.h"
+#include <QDebug>
 
 static void setMaxVolume(QDroidUtils *utils)
 {
@@ -15,12 +16,32 @@ static void setDisplayBrightness(QDroidUtils *utils)
     utils->setDisplayBrightness(255);
 }
 
+static void showValues(QDroidUtils *utils)
+{
+    qDebug() << "Master volume:" << utils->masterVolume();
+    qDebug() << "Master mute:" << utils->masterMute();
+    qDebug() << "System audio volume:" << utils->streamVolume(QDroidUtils::SystemAudioStream);
+    qDebug() << "System audio mute:" << utils->streamMute(QDroidUtils::SystemAudioStream);
+    qDebug() << "Music audio volume:" << utils->streamVolume(QDroidUtils::MusicAudioStream);
+    qDebug() << "Music audio mute:" << utils->streamMute(QDroidUtils::MusicAudioStream);
+    qDebug() << "Notification audio volume:" << utils->streamVolume(QDroidUtils::NotificationAudioStream);
+    qDebug() << "Notification audio mute:" << utils->streamMute(QDroidUtils::NotificationAudioStream);
+    qDebug() << "EnforcedAudible audio volume:" << utils->streamVolume(QDroidUtils::EnforcedAudibleAudioStream);
+    qDebug() << "EnforcedAudible audio mute:" << utils->streamMute(QDroidUtils::EnforcedAudibleAudioStream);
+}
+
 int main(int, char *[])
 {
     QDroidUtils utils;
 
+    qDebug() << "Audio settings before change:";
+    showValues(&utils);
+
     setMaxVolume(&utils);
     setDisplayBrightness(&utils);
+
+    qDebug() << "Audio settings after change:";
+    showValues(&utils);
 
     return 0;
 }
