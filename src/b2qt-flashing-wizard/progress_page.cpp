da66ed8ce7421506ddeb4e7feea13987c26620c7
Rainer Keller <rainer.keller@theqtcompany.com>
2015-04-15 10:08:27 +0200

    b2qt-flashing-wizard: Hints for users to find error messages more easily
    If the image creation fails the details widget is opened and scrolled to
    bottom automatically. Also the progressbar is disabled to indicate a stop.
    
    Change-Id: I74573a2b19d915ac90a12645e3e806a979f74baf
    Reviewed-by: Rainer Keller <rainer.keller@theqtcompany.com>

--- src/b2qt-flashing-wizard/progress_page.cpp
+++ src/b2qt-flashing-wizard/progress_page.cpp
@@ -26,6 +26,7 @@
 #include <QLayout>
 #include <QProgressBar>
 #include <QPushButton>
+#include <QScrollBar>
 #include <QTextEdit>
 #include <QTimer>
 
@@ -37,13 +38,14 @@ ProgressPage::ProgressPage(QWidget *parent)
     , mTextEdit(new QTextEdit(this))
     , mToggleDetailsButton(new QPushButton(this))
     , mCopyToClipboardButton(new QPushButton(this))
+    , mProgressBar(0)
 {
     setTitle(tr("Disk Creation"));
     setSubTitle(tr("Writing the hardware platform image to the disk"));
     setLayout(new QVBoxLayout(this));
-    QProgressBar *progressBar = new QProgressBar(this);
-    progressBar->setRange(0, 0);
-    layout()->addWidget(progressBar);
+    mProgressBar = new QProgressBar(this);
+    mProgressBar->setRange(0, 0);
+    layout()->addWidget(mProgressBar);
     mProgress->setWordWrap(true);
     mProgress->setText(tr("Preparing the disk device..."));
     layout()->addWidget(mProgress);
@@ -91,6 +93,14 @@ void ProgressPage::failed(const QString &message)
 {
     mFinished = false;
     emit completeChanged();
+    showDetails();
+    mProgressBar->setEnabled(false);
+    mProgressBar->setRange(0, 100);
+    mProgressBar->setValue(100);
+    mProgressBar->setFormat("");
+
+    // Scroll to bottom
+    mTextEdit->verticalScrollBar()->setValue(mTextEdit->verticalScrollBar()->maximum());
     mProgress->setText(message);
 }
 
@@ -110,17 +120,26 @@ void ProgressPage::addDetails(QByteArray newData)
     mTextEdit->append(QString::fromLocal8Bit(newData));
 }
 
+void ProgressPage::showDetails()
+{
+    mTextEdit->show();
+    mCopyToClipboardButton->show();
+    mToggleDetailsButton->setText(tr("Hide details"));
+}
+
+void ProgressPage::hideDetails()
+{
+    mTextEdit->hide();
+    mCopyToClipboardButton->hide();
+    mToggleDetailsButton->setText(tr("Show details"));
+}
+
 void ProgressPage::toggleDetails()
 {
-    if (mTextEdit->isHidden()) {
-        mTextEdit->show();
-        mCopyToClipboardButton->show();
-        mToggleDetailsButton->setText(tr("Hide details"));
-    } else {
-        mTextEdit->hide();
-        mCopyToClipboardButton->hide();
-        mToggleDetailsButton->setText(tr("Show details"));
-    }
+    if (mTextEdit->isHidden())
+        showDetails();
+    else
+        hideDetails();
 }
 
 void ProgressPage::copyDetailsToClipboard()
