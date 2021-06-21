c0c3066d25678a92bcb2932b6595d019e9613d27
Dan Ackers <dan.ackers@qt.io>
2021-06-21 11:19:40 +0300

    Port qtdeviceutilities to use declarative types
    Task-number: QTBUG-94426
    Change-Id: I9ac17f38eccff96e25a6c1d40998b222d349274d
    Reviewed-by: Teemu Holappa <teemu.holappa@qt.io>

--- src/settingsui/settingsuiplugin/plugin_p.h
+++ src/settingsui/settingsuiplugin/plugin_p.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2019 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the Device Utilities module of the Qt Toolkit.
@@ -26,23 +26,31 @@
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
-#ifndef NETWORKSETTINGSPLUGIN_PLUGIN_H
-#define NETWORKSETTINGSPLUGIN_PLUGIN_H
+#ifndef SETTINGSUIPLUGIN_H
+#define SETTINGSUIPLUGIN_H
+#include <qqmlextensionplugin.h>
+#include <qqmlengine.h>
 
-#include <QQmlExtensionPlugin>
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of other Qt classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
 
 QT_BEGIN_NAMESPACE
 
-class NetworkSettingsQmlPlugin : public QQmlExtensionPlugin
+struct SettingsUiComponent
 {
-    Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
-
-public:
-    void registerTypes(const char *uri) override;
+    Q_GADGET
+    QML_NAMED_ELEMENT(SettingsUI)
+    QML_ADDED_IN_VERSION(2, 0)
 };
 
 QT_END_NAMESPACE
 
-#endif // NETWORKSETTINGSPLUGIN_PLUGIN_H
-
+#endif //SETTINGSUIPLUGIN_H
