624990fd9a2de4071d8e6e0629cf43d1348ee7c9
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-06-26 07:56:48 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the source files. Examples, tests, documentation,
    or build files are not updated.
    
    Task-number: QTBUG-136484
    Change-Id: I963e6300d228f265673c98b73052a823dd9ccef3
    Reviewed-by: Pasi Petäjäjärvi <pasi.petajajarvi@qt.io>

--- src/networksettings/connman/qnetworksettingsinterface_p.cpp
+++ src/networksettings/connman/qnetworksettingsinterface_p.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 #include "qnetworksettingsinterface_p.h"
 #include "connman_technology_interface.h"
 #include "connmancommon.h"
