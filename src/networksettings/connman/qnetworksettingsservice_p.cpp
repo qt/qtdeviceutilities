88071aaf8773fce055b24cdfa0d10f89f4c826b2
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-11-04 13:09:37 +0200

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit 624990fd9a2de4071d8e6e0629cf43d1348ee7c9.
    
    The revert is needed for the Qt 6.8 LTS opensource releases.
    They cannot contain commercial-only SPDX license identifiers.
    
    Change-Id: I3d8488330b625cbb5321763519fed790fc53c979
    Reviewed-by: Pasi Petäjäjärvi <pasi.petajajarvi@qt.io>

--- src/networksettings/connman/qnetworksettingsservice_p.cpp
+++ src/networksettings/connman/qnetworksettingsservice_p.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 #include "qnetworksettingsservice_p.h"
 #include "connman_service_interface.h"
 #include "connmancommon.h"
